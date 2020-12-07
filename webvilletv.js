9a50625beace94fdeecc76654d1b0c36dcfa4ae0
gary909 <garywhite61@hotmail.com>
2020-12-07 15:30:21 +0100

    code updates

--- webvilletv.js
+++ webvilletv.js
@@ -3,11 +3,33 @@ var playlist;
 var video;
 
 window.onload = function() {
-    playlist = ["video/preroll.mp4", "video/areyoupopular.mp4", "video/destinationearth.mp4"];
+    playlist = ["video/preroll", "video/areyoupopular", "video/destinationearth"];
     video = document.getElementById("video");
     video.addEventListener("ended", nextVideo, false);
 
-    video.src = playlist[position];
+    video.src = playlist[position] + getFormatExtension();
     video.onload();
     video.play();
+    // alert("Playing " + video.currentSrc);
+}
+
+function nextVideo() {
+    position++;
+    if (position >= playlist.length) {
+        position = 0;
+    }
+    video.src = playlist[position] + getFormatExtension();
+    video.load();
+    video.play();
+    // alert("Playing " + video.currentSrc);
+}
+
+function getFormatExtension() {
+    if (video.canPlayType("video/mp4") != "") {
+        return ".mp4";
+    } else if (video.canPlayType("video/webm") !="") {
+        return ".webm";
+    } else if (video.canPlayType("video/ogg") !="") {
+        return ".ogv";
+    }
 }
