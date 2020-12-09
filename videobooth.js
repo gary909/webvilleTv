d55da47c814b9b3be688a9d962f7993b55782289
gary909 <garywhite61@hotmail.com>
2020-12-09 14:37:03 +0100

    code updates

--- videobooth.js
+++ videobooth.js
@@ -3,4 +3,17 @@ window.onload = function() {
     for (var i = 0; i < controlLinks.length; i++) {
         controlLinks[i].onclick = handleControl;
     }
+
+    var effectLinks = document.querySelectorAll("a.effect");
+    for (var i = 0; i < effectLinks.length; i++) {
+        effectLinks[i].onclick = setEffect;
+    }
+
+    var videoLinks = document.querySelectorAll("a.videoSelection");
+    for (var i = 0; i < videoLinks.length; i++) {
+        videoLinks[i].onclick = setVideo;
+    }
+
+    pushUnpushButtons("video1", []);
+    pushUnpushButtons("normal", []);
 }
